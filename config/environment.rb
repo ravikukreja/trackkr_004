a723f60ba68942dbd9a31ab36784ff379ff46145
Manikandan K <manikandan.nitt@gmail.com>
2011-09-11 21:39:59 +0530

    Friend Search With Sending the Mail to the selected User  Gmail Account Created for Trackkr. i.e trackkr@gmail.com, password:trackkr@123.  Mail configuration given in config/environment.rb  PaperClip added for image uploadation

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Trackkr002::Application.initialize!
+ActionMailer::Base.smtp_settings = {
+    :tls => true,
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "GMAIL",
+    :authentication => :plain,
+    :user_name => "trackkr",
+    :password => "trackkr@123"
+  }
