1d5de1c9076ada905ac658952176003920c65632
Manikandan K <manikandan.nitt@gmail.com>
2011-10-15 20:22:10 +0530

    Add Friend Concept fixed by sending mail to friend and Add to cart after sending the mail

--- app/mailer/user_mailer.rb
+++ app/mailer/user_mailer.rb
@@ -1,10 +1,16 @@
 class UserMailer < ActionMailer::Base
   default :from => "trackkr@gmail.com"
   
-  def invite_to_product(frd_user,product,content)
-    @frd_user = frd_user
-    @content = content
-    mail(:to => "#{@frd_user.username} <#{@frd_user.email}>", :subject => "Please visit this Product #{product.name} in Trackkr" )
+  def invite_to_product(frd_obj,content)
+    @frd_user = frd_obj
+    @content = content + ' Activate the product: ' + friend_url(:id=>frd_obj.authenticity_token,:host=>'trackkr.com')  
+    mail(:to => "#{@frd_user.friend.username} <#{@frd_user.friend.email}>", :subject => "Please visit this Product #{frd_obj.product.name} in Trackkr" )
+  end
+  
+  def invite_to_trackkr(user_email,product,content)
+    @frd_user = user_email
+    @content = content + ' Activate the product: ' + "http://trackkr.com/user_sessions/new"
+    mail(:to => @frd_user, :subject => "Please visit this Product #{product.name} in Trackkr" )
   end
 end
 
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,6 +1,7 @@
 class Friend < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
+  belongs_to :friend, :class_name => "User"
 end
 
 
