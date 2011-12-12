fea4eda05bb07b3230e989a9ea14882676836a2d
Manish <manishkukreja@gmail.com>
2011-12-12 21:15:57 +0530

    bittu_changes_12_dec

--- app/models/user_product_plan_data.rb
+++ app/models/user_product_plan_data.rb
@@ -9,4 +9,13 @@ class UserProductPlanData < ActiveRecord::Base
     where("training_date = ? and user_product_plan_id = ?", date, user_product_plan_id).sum(:plan_distance)
   end
 
+  def self.user_actual_speed(date, user_product_plan_id)
+    where("training_date = ? and user_product_plan_id = ?", date, user_product_plan_id).sum(:actual_speed)
+  end
+  
+  def self.user_plan_speed(date, user_product_plan_id)
+    where("training_date = ? and user_product_plan_id = ?", date, user_product_plan_id).sum(:plan_speed)
+  end
+
+
 end
