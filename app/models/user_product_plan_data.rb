522ac43f4fce11d3c28a6b660fbae708888a64d5
Manikandan K <manikandan.nitt@gmail.com>
2011-12-26 23:57:18 +0530

    User Product Plan Data filtered by last 14 days and unfilled actual data

--- app/models/user_product_plan_data.rb
+++ app/models/user_product_plan_data.rb
@@ -1,5 +1,12 @@
 class UserProductPlanData < ActiveRecord::Base
   belongs_to :user_product_plan
+ scope :by_days, lambda{ |no_days|
+     where("training_date >= ?", no_days.days.ago)  
+       }
+         
+	   scope :unfilled_actual_data,lambda {
+	       where("actual_distance is NULL and actual_speed is NULL and actual_time is NULL" )
+	         }
  
   def self.user_actual_distance(date, user_product_plan_id)
     where("training_date = ? and user_product_plan_id = ?", date, user_product_plan_id).sum(:actual_distance)
