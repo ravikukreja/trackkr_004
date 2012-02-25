9d81562be686cddcdef758c97b4b031246408c1d
ravikukreja <ravi.kukreja@gmail.com>
2012-02-24 23:52:26 -0800

    adding_test_database

--- app/helpers/user_product_actual_datas_helper.rb
+++
@@ -1,7 +0,0 @@
-module UserProductActualDatasHelper
-  
-  def new_or_edit_path(training_date)
-     check_if_record_exists ? edit_user_product_actual_data_path(2) : new_user_product_actual_data_path
-  end 
-  
-end
--- app/models/user_product_actual_data.rb
+++
@@ -1,8 +0,0 @@
-class UserProductActualData < ActiveRecord::Base
-  belongs_to :user_product
-  
- def input_plan_data_into_user_table
-   return true
- end
- 
-end
