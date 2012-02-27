75d8eee60178cf29881ce37b3855b9b471d1d836
ravikukreja <ravi.kukreja@gmail.com>
2012-02-27 08:48:25 -0800

    misc_testing

--- test/unit/user_product_plan_test.rb
+++ test/unit/user_product_plan_test.rb
@@ -1,8 +1,29 @@
 require 'test_helper'
 
 class UserProductPlanTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+# Start date and End date should be date
+  test "start date and end date should be date" do
+    user_product_plan = UserProductPlan.new 
+    user_product_plan_start_date = "abc"
+    user_product_plan_end_date = "ab"
+    assert user_product_plan.invalid? "Only numerical value is allowed"
   end
+  
+  #Start date should be less then End date
+  
+  test "start date should be less then end date" do
+    user_product_plan = UserProductPlan.new(:start_date => '01/01/2012',
+                                            :end_date => '01/02/2012')
+    assert user_product_plan.end_date > user_product_plan.start_date
+  end
+
+
+  test "product plan id exists" do
+    user_product_plan = UserProductPlan.new(:start_date => '01/02/2012',
+                                            :end_date => '02/02/2012'
+                                            )
+    assert user_product_plan.invalid?
+   end
+  
 end
